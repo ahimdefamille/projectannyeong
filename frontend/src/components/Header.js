33e411fbb563405f2d72202986b8cc2d26dc26d2
Saul <saul@paymentwall.com>
2024-10-22 16:50:38 +0700

    Update 22/10

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -40,7 +40,7 @@ const Header = () => {
 
   const fetchUserDetails = async (userId) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/users/${userId}`, {
+      const response = await fetch(`https://https://projectannyeong.onrender.com/api/users/${userId}`, {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`,
